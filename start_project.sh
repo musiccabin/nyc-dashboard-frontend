265372d4017bb037ced61a2200cd1de0f30def1d
Susannah Sun <thisis.susannahsun@gmail.com>
2026-01-30 16:35:40 -0800

    Make start_project.sh cleanly shut down servers on terminal exit

--- start_project.sh
+++ start_project.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -e
+
+# Kill servers after terminal exits
+trap "kill 0" EXIT
+
 # Activate Python virtual environment
 cd backend || exit
 source venv/bin/activate
 
-# Kill previous backend if running
+# Kill previous servers if running
 echo "Stopping any existing servers..."
 lsof -ti:{8000,5173} | xargs -r kill -9
 
@@ -23,3 +28,5 @@ code -n backend
 code -n frontend
 
 echo "Ready to work!"
+
+wait
